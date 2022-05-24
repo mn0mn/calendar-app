53ff636fa32fca0d80106c4cc58d9cb9cad90a68
Mohammad Naseri <mnaseri841@gmail.com>
2022-05-24 18:50:20 +0430

    improved the ol' stupid qotd service

--- lib/Services/qotd_service.dart
+++ lib/Services/qotd_service.dart
@@ -1,21 +1,30 @@
 import 'dart:convert';
+import 'dart:developer';
 
 import 'package:calendar_app/Models/qoute_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class QuoteService {
   static const url = 'https://zenquotes.io/api/today';
 
   Future<Quote> getQuote() async {
-    var response = await GetConnect().get(url);
-    if (response.isOk) {
-      var body = jsonDecode(json.encode(response.body[0]));
-      return Quote(body['a'], body['q']);
-    } else {
-      return Quote(
-        'Error getting the Quote',
-        '${response.statusCode}',
-      );
+    bool ok = false;
+    var i = 0;
+    while (!ok && i <= 5) {
+      log('quote attempt $i');
+      var req = await GetConnect().get(url);
+      if (req.isOk) {
+        ok = true;
+        return Quote.fromJson(req.body[0]);
+      }
     }
+    Get.dialog(
+        const AlertDialog(
+          content: Text('Oops :)'),
+        ),
+        barrierDismissible: true,
+        useSafeArea: true);
+    return Quote(':(', 'Error getting the Quote');
   }
 }
