a1c5bbc45b9610656cf0dd3ec15d4bdc8063a562
Mohammad Naseri <mnaseri841@gmail.com>
2022-05-24 18:36:53 +0430

    added fromJson()

--- lib/Models/qoute_model.dart
+++ lib/Models/qoute_model.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 class Quote {
   final String auther, quote;
 
   Quote(this.auther, this.quote);
+
+  factory Quote.fromJson(dynamic json) {
+    return Quote(json['a'], json['q']);
+  }
 }
