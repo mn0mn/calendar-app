4a4038652657db575259fa048e0e1f0d8ab43111
Mohammad <mnaseri841@gmail.com>
2022-07-25 16:09:42 +0430

    fixed the APOD function

--- lib/Services/apod_service.dart
+++ lib/Services/apod_service.dart
@@ -4,12 +4,24 @@ class Apod {
   static final Rx<String> image = ''.obs;
   static final Rx<String> title = ''.obs;
   Future<void> getImage() async {
-    await GetConnect()
-        .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-        .then((value) => value.body)
-        .then((value) {
-      title.value = value['title'];
-      image.value = value['url'];
-    });
+    try {
+      await GetConnect()
+          .get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+          .then((value) => value.body)
+          .then(
+        (value) {
+          if (value != null) {
+            title.value = value['title'];
+            image.value = value['url'];
+          }
+        },
+      );
+    } catch (e) {
+      Get.defaultDialog(
+        title: 'Error getting the APOD',
+        middleText: e.toString(),
+      );
+      getImage();
+    }
   }
 }
